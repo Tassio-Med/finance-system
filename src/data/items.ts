607705f61bb74a5560ec2712e879e926e4759c6f
Tassio Med <tassiohfmed@gmail.com>
2022-10-24 21:00:27 -0300

    :sparkles: feat:  arquivo de dados a serem utilizados criados e tipagem adicionada

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+  { date: new Date(2022, 9, 24), category: 'food', title: 'Giraffas', value: 32.12 },
+  { date: new Date(2022, 9, 24), category: 'food', title: 'Grilleto', value: 30 },
+  { date: new Date(2022, 9, 16), category: 'rent', title: 'Aluguel Casa', value: 32.12 },
+  { date: new Date(2022, 9, 20), category: 'salary', title: 'Salário MELI', value: 5400 },
+];
