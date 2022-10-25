4aa8357c1902929694ea258c4972f61325400fce
Tassio Med <tassiohfmed@gmail.com>
2022-10-24 21:29:57 -0300

    :sparkles: feat:  criado arquivo de categorias com a tipagem importada

---
+++ src/data/categories.ts
@@ -0,0 +1,7 @@
+import { Category } from '../types/Category';
+
+export const categories: Category = {
+  food: { title: 'Alimentação', color: 'blue', expense: true },
+  rent: { title: 'Alimentação', color: 'yellow', expense: true },
+  salary: { title: 'Alimentação', color: 'green', expense: false },
+}
