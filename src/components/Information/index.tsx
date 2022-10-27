48df17d8f57e7eec37691562840b09a9d9268753
Tassio-Med <tassiohfmed@gmail.como>
2022-10-27 17:42:15 -0300

    :sparkles: feat: componente Information criado com seu arquivo de estilização.

---
+++ src/components/Information/index.tsx
@@ -0,0 +1,15 @@
+import * as C from './styles';
+
+export const Information = () => {
+  return (
+    <C.Container>
+      <C.MonthArea>
+        <C.ArrowLeft></C.ArrowLeft>
+        <C.ArrowRight></C.ArrowRight>
+      </C.MonthArea>
+      <C.Resume>
+        
+      </C.Resume>
+    </C.Container>
+  );
+}
---
+++ src/components/Information/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: #FFF;
+  padding: 20px;
+  box-shadow: 0px 0px 5px #CCC;
+  border-radius: 10px;
+  margin-top: -30px;
+`;
