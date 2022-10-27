06f35a18e2bc8c5f05ec64967baf25bf39008b64
Tassio-Med <tassiohfmed@gmail.como>
2022-10-27 18:35:30 -0300

    :sparkles: feat: estilizaçãon da página iniciada

--- src/components/Information/styles.ts
+++ src/components/Information/styles.ts
@@ -6,4 +6,30 @@ export const Container = styled.div`
   box-shadow: 0px 0px 5px #CCC;
   border-radius: 10px;
   margin-top: -30px;
+  display: flex;
+  align-items: center;
 `;
+
+export const MonthArea = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+export const MonthName = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+
+export const Arrow = styled.div`
+  width: 40px;
+  text-align: center;
+  font-size: 25px;
+  cursor: pointer;
+`;
+
+export const Resume = styled.div`
+  flex: 2;
+  display: flex;
+`;
+
